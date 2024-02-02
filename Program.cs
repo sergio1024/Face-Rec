8eed857c8ed858379f1601655ab004549a326afc
sergio1024 <137495954+sergio1024@users.noreply.github.com>
2024-02-02 14:04:25 +0000

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace FaceRec
+{
+    static class Program
+    {
+        /// <summary>
+        /// Primary entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FrmPrincipal());
+        }
+    }
+}
+//
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// The general information about an assembly is controlled by the following
+// Set of attributes. Change these attributes to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Face-Rec")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Face-Rec")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// If you set ComVisible to false, the types in this assembly will not be visible
+// for COM components. If you need to access a type of this assembly from
+// COM, set the ComVisible attribute to true in this type.
+[assembly: ComVisible(false)]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor version
+//      Build Number
+//      Revision
+//
+// You can specify all values or set the build and revision version numbers as the default
+// using the asterisk ('*'), as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
